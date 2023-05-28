302dde3732850483f639b600b10d0609ebeba80d
Pierre Troger <ptroger@student.42.fr>
2023-05-28 17:51:36 +0200

    ALL messages

--- CPP09/ex02/PmergeMe.cpp
+++ CPP09/ex02/PmergeMe.cpp
@@ -41,32 +41,23 @@ void	PmergeMe::mergeInsertVectorAlgorithm(int chunks, int size)
 	int current;
 	std::vector<int> tmp = _vector;
 
-	for (int l = 0; l < _vector.size(); l++)
-		std::cout << _vector[l] << " ";
-	std::cout << std::endl;
 	while (i < size - chunks + 1)
 	{
 		j = i + chunks;
-		while (j < i + chunks * 2) // could add isSorted(i, chunksize);
+		while (j < i + chunks * 2)// could add isSorted(i, chunksize);
 		{
-			current = _vector[j]; // int copy cuz we change the value of _vector
+			current = _vector[j];// int copy cuz we change the value of _vector
 			int k = j; 
-			std::cout << "k = " << k << " i = " << i << " j = " << j << " _vector[k] = " << _vector[k] << " _vector[k - 1] = " << _vector[k - 1] << std::endl;
-			for (; k > i && current <= _vector[k - 1] ;) // https://www.softwaretestinghelp.com/merge-sort/
+			for (; k > i && current <= _vector[k - 1] ;)// https://www.softwaretestinghelp.com/merge-sort/
 			{
 				k--;
-				// std::cout << "im here" << std::endl;
 				_vector[k + 1] = _vector[k];
 			}
 			_vector[k] = current;
 			j++;
 		}
-		std::cout << "changement de chunk" << std::endl;
 		i += chunks * 2;
 	}
-	std::cout << "after sort of chunks " << chunks << " = ";
-	for (int l = 0; l < _vector.size(); l++)
-		std::cout << _vector[l] << " ";
 	std::cout << std::endl;
 	if (chunks < size + 1)
 	{
@@ -84,10 +75,8 @@ void	PmergeMe::mergeInsertListAlgorithm(int chunks, int size)
     int current;
     std::list<int> tmp = _list;
 
-    std::cout << "before sort list of chunks " << chunks << " = ";
-    for (std::list<int>::iterator it = _list.begin(); it != _list.end(); it++)
-        std::cout << *it << " ";
-    std::cout << std::endl;
+    //for (std::list<int>::iterator it = _list.begin(); it != _list.end(); it++)
+    //    std::cout << *it << " ";
     while (i < size - chunks + 1)
     {
         j = i + chunks;
@@ -122,7 +111,6 @@ void	PmergeMe::mergeInsertListAlgorithm(int chunks, int size)
 void	PmergeMe::vectorSort(char **input)
 {
 	int i = 0;
-	int	total_time;
 	int	beginning = time_now_in_us();
 
 	while (input[i])
@@ -132,12 +120,10 @@ void	PmergeMe::vectorSort(char **input)
 	}
 	for (int l = 0; l < _vector.size(); l++)
 		std::cout << _vector[l] << " ";
-	std::cout << "Hello" << std::endl;
 	int	beginningMergeSortAlgoVector = time_now_in_us();
 	mergeInsertVectorAlgorithm(1, _vector.size() - 1);
 	_timeFullVector = time_now_in_us() - beginning;
 	_timeMergeSortVector = time_now_in_us() - beginningMergeSortAlgoVector;
-	std::cout << "vectorSort took " << total_time << " microseconds" << std::endl;
 }
 
 void	PmergeMe::listSort(char **input)
